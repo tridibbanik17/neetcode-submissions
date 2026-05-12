9532bedb8a7d3f3de44252f0268a263012ac760e
Tridib Banik <tridib.perfect@gmail.com>
2026-05-11 23:58:40 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            key = frozenset(Counter(s).items())
+            groups[key].append(s)
+        return list(groups.values())
