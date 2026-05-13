f9769b10b13e2c16376438d11d992d0f6723acc6
Tridib Banik <tridib.perfect@gmail.com>
2026-05-13 01:00:39 -0400

    Improve comments in topKFrequent function

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -10,16 +10,16 @@
 # ============================================================
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {} # map count of each element to a list of elements that occurs that mant times
-        freq = [[] for i in range(len(nums) + 1)]
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-        for n, c in count.items():
-            freq[c].append(n)
+        count = {} # {n:c} where n = each distinct element of nums, c = counted number of element n
+        freq = [[] for i in range(len(nums) + 1)] # initialize empty sublists 
+        for num in nums: 
+            count[num] = count.get(num, 0) + 1 # find the total count of each distinct element of nums
+        for n, c in count.items(): # count.items = dict_items[(n1,c1), (n2,c2)]
+            freq[c].append(n) # in each sublist, when applicable, add element 
 
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
+        for i in range(len(freq) - 1, 0, -1): # start iterating from the end and gradually move left
+            for n in freq[i]: 
                 res.append(n)
                 if len(res) == k:
                     return res
