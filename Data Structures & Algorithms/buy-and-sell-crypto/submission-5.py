fc3be679c0c2d917c640e213f2a6e2d635870536
Tridib Banik <tridib.perfect@gmail.com>
2026-05-23 00:09:29 -0400

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = l + 1
+        max_profit = 0
+        while r != len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(profit, max_profit)
+            else:
+                l = r
+            r += 1
+        return max_profit
