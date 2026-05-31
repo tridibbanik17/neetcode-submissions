ff407e0ac95b529e18da0c7a9014521225e04e61
Tridib Banik <tridib.perfect@gmail.com>
2026-05-30 22:35:11 -0400

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.length = 0
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.length += 1
+
+    def pop(self) -> None:
+        del self.stack[-1]
+        self.length -= 1
+
+    def top(self) -> int:
+        last_element = self.stack[self.length - 1]
+        return last_element
+
+    def getMin(self) -> int:
+        # return min(self.stack)
+        min_element = self.stack[0]
+        for i in range(len(self.stack)):
+            if self.stack[i] < min_element:
+                min_element = self.stack[i]
+        return min_element
+        
