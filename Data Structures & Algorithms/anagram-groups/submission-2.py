9e677d51b08ea2aa25043b1800f75ed87325b998
Tridib Banik <tridib.perfect@gmail.com>
2026-06-11 21:57:22 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,10 @@
+# Optimal solution: O(m*n), where m = each string size, n = strs list size
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) # map character count to list of anagrams
+        for string in strs:
+            count = [0]*26 # a list of 26 0s
+            for char in string:
+                count[ord(char) - ord("a")] += 1 # after reading each char in s string, increment the counter for that string
+            res[tuple(count)].append(string) # passing count as the key will raise TypeError because count is a list and list is mutable and key can never be mutable
+        return list(res.values()) # return hashmap values
